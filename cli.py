ed9e32bfdc558f98e7a155719d33ccf6dabf1434
Neill Engelbrecht <engelbrecht.neill979@gmail.com>
2021-03-31 16:34:59 +0200

    Create cli.py

---
+++ cli.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import os
+import argparse as argp
+
+def CLI_Prog():
+    def __init__(self):
+        parser = argp.ArgumentParser(
+            description="CLI tool for generating passwords",
+            usage="""Usage: <command> [<args>]"""
+        )
