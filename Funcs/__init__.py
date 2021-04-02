065e402ca89d10e8033b8652aed94e9cd33e285c
Neill Engelbrecht <engelbrecht.neill979@gmail.com>
2021-04-02 20:19:55 +0200

    Small patch

--- Funcs/__init__.py
+++ Funcs/__init__.py
@@ -3,6 +3,6 @@
 import re, os
 
 def getpwd():
-    path_pattern = re.compile(r"\/?\w\:?(\\|\/)Users(\\|\/)[a-zA-Z0-9]+(\\|\/)?")
+    path_pattern = re.compile(r"\/?\w\:?(\\|\/)Users(\\|\/)[a-zA-Z0-9]+")
     path = path_pattern.match(os.getcwd())
     return path.group()
