cb29e98d67e33d61f0c0135607ca6bb2aa12efa2
Neill Engelbrecht <engelbrecht.neill979@gmail.com>
2021-04-04 20:26:50 +0200

    Added tri as the keyword

--- cli.py
+++ cli.py
@@ -4,29 +4,47 @@ import os, sys, Funcs, pyperclip
 from random import randint
 import argparse as argp
 
-class CLI_Prog():
+class CLI_Tri():
     def __init__(self):
+        
+        sysInput = sys.argv
+        
+        if sysInput[1] == "tri":
+        
+            cliCommand = sysInput[1:3]
+            
+            if cliCommand[-1][0] != "-" and cliCommand[-1] != "tri":
+                keyw, com = cliCommand
+                command = keyw+com
+            else:
+                command = cliCommand[0]
+            
+            getattr(self, command)()
+        else:
+            pass
+    
+    def tri(self):
         parser = argp.ArgumentParser(
             description="CLI tool for generating passwords",
-            usage="""cli.py <command> [<args>]\n\nThese are common Gen commands:\n  gen\tGenerates password"""
+            usage=f"""usage: tri [-v | --version] [-h | --help]\n\t{' '*3}<command> [<args>]\n\nThese are the most common Tri commands:\n  gen\tGenerates a password\n"""
         )
-        parser.add_argument("command", help="The command to run")
+        parser.add_argument("-v", "--version", action="store_true", help="Gets your instalation version of Tri")
+        args = parser.parse_args(sys.argv[2:]) # it gets the rest of the optional arguments after tri
         
-        # it gets the first argument after cli.py
-        args = parser.parse_args(sys.argv[1:2])  # PS. could also be sys.argv[1]
-
-        # gets the name of the func specified in command and calls it
-        getattr(self, args.command)()
-    
-    def gen(self):
+        if args.version:
+            print("You still have to implement this\n")
+        else:
+            print(parser.usage)
+            
+    def trigen(self):
         parser = argp.ArgumentParser(
-            description="Generates password",
-            usage="cli.py gen [<args>]"
+            description="Generates a password and copies it to your clipboard",
+            usage="""usage: tri gen [<options>]\n\t-s, --symbols\tFile with the symbols you want to be used\n\t-l, --length\tThe length you want the password to be\n\t-v, --verbose\tShow more information"""
         )
         parser.add_argument("-s", "--symbols", type=str, default="default", help="File with the symbols you want to be used")
         parser.add_argument("-l", "--length", type=int, default=15, help="The length you want the password to be")
-        parser.add_argument("-v", "--verbose", action="store_true", help="Gives more information")
-        args = parser.parse_args(sys.argv[2:])  # it gets the rest of the optional arguments after gen
+        parser.add_argument("-v", "--verbose", action="store_true", help="Show more information")
+        args = parser.parse_args(sys.argv[3:])  # it gets the rest of the optional arguments after gen
         
         passSyms = []
         
@@ -37,11 +55,11 @@ class CLI_Prog():
             pwd = "".join(passSyms[randint(0, len(passSyms)-1)] for _ in range(args.length))
             
             if args.verbose:
-                print(f"Password generated is:\t{pwd}")
+                print(f"Password generated is:\t{pwd}\n")
             
             pyperclip.copy(pwd)
         except FileNotFoundError:
-            print("File specified does not exist")
+            print("File specified does not exist\n")
 
 if __name__=="__main__":
-    CLI_Prog()
+    CLI_Tri()
